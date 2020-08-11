47f9912c3ed84b5cbfadf4d6ae19912cf0164ee3
Kenneth Pham <kennethpham98@gmail.com>
2020-08-11 14:41:05 -0700

    put the function definitions from event.h into event.cpp

--- event.cpp
+++ event.cpp
@@ -1,1 +1,24 @@
 #include "event.h"
+
+//Event::Event(Character& ch, Inventory& inv, std::string vS = " .") : character(ch), i(inv), isVisited(false), currentlyOccupied(false), notVisitedSymbol(" ?"), visitedSymbol(vS) {}
+
+std::string Event::getSymbol() const {
+    return isVisited ? visitedSymbol : notVisitedSymbol;
+}
+
+void Event::performEvent() {
+    isVisited = true;
+    std::cout << "An event has occured!" << std::endl;
+}
+
+bool Event::getIsVisited() const {
+    return isVisited;
+}
+
+void Event::visit() {
+    isVisited = true;
+}
+
+void Event::unVisit() {
+    isVisited = false;
+}
--- event.h
+++ event.h
@@ -16,25 +16,14 @@ public:
 	Event() = delete; //forced to use overloaded c-tor
 	Event(Character& ch, Inventory& inv, std::string vS = " .") : character(ch), i(inv), isVisited(false), currentlyOccupied(false), notVisitedSymbol(" ?"), visitedSymbol(vS) {}
 
-	virtual std::string getSymbol() const {
-		return isVisited ? visitedSymbol : notVisitedSymbol;
-	}
+	virtual std::string getSymbol() const;
 
-	virtual void performEvent() {
-		isVisited = true;
-		std::cout << "An event has occured!" << std::endl;
-	}
+	virtual void performEvent();
 
-	bool getIsVisited() const {
-		return isVisited;
-	}
+	bool getIsVisited() const;
 
-	void visit() {
-		isVisited = true;
-	}
+	void visit();
 
-	void unVisit() {
-		isVisited = false;
-	}
+	void unVisit();
 };
 #endif
